06bb39539c9863445f779569a847188f04f0ae2d
Rafael Alberto Larios <albertolarios@me.com>
2023-01-07 09:00:06 -0600

    Add task on TaskOverview's state on TaskForm submit

--- src/components/Overview.js
+++ src/components/Overview.js
@@ -3,6 +3,7 @@ import React from 'react';
 class TaskOverview extends React.Component {
   constructor(props) {
     super(props);
+    this.onTaskSubmit = this.onTaskSubmit.bind(this);
     this.state = {
       tasks: [
         {
@@ -33,10 +34,18 @@ class TaskOverview extends React.Component {
     };
   }
 
+  onTaskSubmit(e) {
+    e.preventDefault();
+    const data = new FormData(e.target);
+    const newTask = {title: data.get('title'), date: new Date()};
+    const tasks = [...this.state.tasks].concat(newTask);
+    this.setState({tasks: tasks});
+  }
+
   render() {
     return (
       <div id='task-overview'>
-        <TaskForm />
+        <TaskForm onSubmit={this.onTaskSubmit}/>
         <TaskList tasks={this.state.tasks} />
       </div>
     );
@@ -47,9 +56,9 @@ class TaskForm extends React.Component {
 
   render() {
     return (
-      <form>
+      <form onSubmit={this.props.onSubmit} >
         <label htmlFor='add-task'>Add Taks</label>
-        <input id='add-task' type='text' required />
+        <input id='add-task' type='text' name='title' required />
         <input type='submit' value='Submit' />
       </form>
     );
