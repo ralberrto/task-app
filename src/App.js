9a6d03dc802c8f87ef9210bc5253f33e559a9e19
Rafael Alberto Larios <albertolarios@me.com>
2023-01-06 20:06:29 -0600

    Refactor App component to attach state to it

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
+import React from 'react';
 import './App.css';
 import TaskList from './components/Overview';
 
-function App() {
-  return (
-    <div className="App">
-      <TaskList />
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+
+    }
+  }
+  render() {
+    return (
+      <div className="App">
+        <TaskList />
+      </div>
+    );
+  }
 }
 
 export default App;
