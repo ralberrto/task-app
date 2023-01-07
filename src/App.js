98787e93bd962aeee572c1cd92f880c511e47ddb
Rafael Alberto Larios <albertolarios@me.com>
2023-01-07 07:21:07 -0600

    Create TaskOverview component and transfer state from App to it for Scalability

--- src/App.js
+++ src/App.js
@@ -1,44 +1,13 @@
 import React from 'react';
 import './App.css';
-import { TaskForm, TaskList } from './components/Overview';
+import TaskOverview from './components/Overview';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      tasks: [
-        {
-          title: 'Check the terminal of the car\'s battery',
-          date: new Date()
-        },
-        {
-          title: 'Do purchases checklist',
-          date: new Date()
-        },
-        {
-          title: 'Get money from the back',
-          date: new Date()
-        },
-        {
-          title: 'Actually make purchases',
-          date: new Date()
-        },
-        {
-          title: 'Ship purchases',
-          date: new Date()
-        },
-        {
-          title: 'Make customer\'s receipt',
-          date: new Date()
-        }
-      ],
-    };
-  }
+
   render() {
     return (
       <div className="App">
-        <TaskForm />
-        <TaskList tasks={this.state.tasks}/>
+        <TaskOverview />
       </div>
     );
   }
--- src/components/Overview.js
+++ src/components/Overview.js
@@ -1,5 +1,48 @@
 import React from 'react';
 
+class TaskOverview extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tasks: [
+        {
+          title: 'Check the terminal of the car\'s battery',
+          date: new Date()
+        },
+        {
+          title: 'Do purchases checklist',
+          date: new Date()
+        },
+        {
+          title: 'Get money from the bank',
+          date: new Date()
+        },
+        {
+          title: 'Actually make purchases',
+          date: new Date()
+        },
+        {
+          title: 'Ship purchases',
+          date: new Date()
+        },
+        {
+          title: 'Make customer\'s receipt',
+          date: new Date()
+        }
+      ],
+    };
+  }
+
+  render() {
+    return (
+      <div id='task-overview'>
+        <TaskForm />
+        <TaskList tasks={this.state.tasks} />
+      </div>
+    );
+  }
+}
+
 class TaskForm extends React.Component {
   render() {
     return (
@@ -27,4 +70,4 @@ class TaskList extends React.Component {
   }
 }
 
-export { TaskForm, TaskList };
+export default TaskOverview;
