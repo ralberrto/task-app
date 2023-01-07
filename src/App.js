10c2dc8a767bd8702332015fa68dd48194da5732
Rafael Alberto Larios <albertolarios@me.com>
2023-01-06 20:52:25 -0600

    Define TaksForm component and render it in App

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import TaskList from './components/Overview';
+import { TaskForm, TaskList } from './components/Overview';
 
 class App extends React.Component {
   constructor(props) {
@@ -37,6 +37,7 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
+        <TaskForm />
         <TaskList tasks={this.state.tasks}/>
       </div>
     );
--- src/components/Overview.js
+++ src/components/Overview.js
@@ -1,5 +1,17 @@
 import React from 'react';
 
+class TaskForm extends React.Component {
+  render() {
+    return (
+      <form>
+        <label htmlFor='add-task'>Add Taks</label>
+        <input id='add-task' type='text' />
+        <input type='submit' value='Submit' />
+      </form>
+    );
+  }
+}
+
 class TaskList extends React.Component {
   render() {
     const tasks = this.props.tasks.map((task, index) => {
@@ -15,4 +27,4 @@ class TaskList extends React.Component {
   }
 }
 
-export default TaskList
+export { TaskForm, TaskList };
