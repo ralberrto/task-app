7033e623dca28214a0ded90cfedda57296aaa9e4
Rafael Alberto Larios <albertolarios@me.com>
2023-01-06 20:14:20 -0600

    Add dummy tasks to App's state for testing purposes

--- src/App.js
+++ src/App.js
@@ -6,8 +6,33 @@ class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-
-    }
+      tasks: [
+        {
+          title: 'Check the terminal of the car\'s battery',
+          date: new Date()
+        },
+        {
+          title: 'Do purchases checklist',
+          date: new Date()
+        },
+        {
+          title: 'Get money from the back',
+          date: new Date()
+        },
+        {
+          title: 'Actually make purchases',
+          date: new Date()
+        },
+        {
+          title: 'Ship purchases',
+          date: new Date()
+        },
+        {
+          title: 'Make customer\'s receipt',
+          date: new Date()
+        }
+      ],
+    };
   }
   render() {
     return (
