d920be26071b758dc700931bb11c81df5ff2239e
Rafael Alberto Larios <albertolarios@me.com>
2023-01-06 20:20:34 -0600

    Remove Task component and references to it

--- src/components/Overview.js
+++ src/components/Overview.js
@@ -1,18 +1,9 @@
 import React from 'react';
 
-class Task extends React.Component {
-  render() {
-    return (
-      <li className="test">Hello, planet {this.props.planet}</li>
-    );
-  }
-}
-
 class TaskList extends React.Component {
   render() {
     return (
       <ul className='task-list'>
-        <Task planet='Mars' />
       </ul>
     );
   }
